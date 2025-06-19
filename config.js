e8ff33fb97b45b1d1ec7cb04081cd8a988a52da0
Nanotect <61177761+Adivise@users.noreply.github.com>
2025-06-19 23:00:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     songsFolder: "E:/osu!/Songs", // path of your osu songs folder like | C:/Adivise/osu!/Songs
     downloadMaxed: 100, // how much beatmap your need to download | it not correctly 100 beatmap *iam lazy to code that*
-    workers: 10, // how many workers to download | it not correctly 100 beatmap *iam lazy to code that*
+    workers: 10, // how many workers to download
     apiKey: "YOUR_API_KEY", // osu!apikey your can get in osu!website
     mode: 0, // mode to download | 0 = Standard, 1 = Taiko, 2 = Catch to beat, 3 = Mania
     starRating_min: 5.00, // star min can set 5.69
